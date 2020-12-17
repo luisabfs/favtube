34df8b586c63c827a8e3949cbd3579f30a0ac697
Luísa Barros <qlullyq@gmail.com>
2020-12-17 13:12:01 -0300

    feat: create auth routes

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -7,7 +7,12 @@ import Favorites from '../pages/Favorites';
 const App = createStackNavigator();
 
 const AppRoutes = () => (
-  <App.Navigator screenOptions={{ headerShown: false }}>
+  <App.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#fff' },
+    }}
+  >
     <App.Screen name="Search" component={Search} />
     <App.Screen name="Favorites" component={Favorites} />
   </App.Navigator>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
 
 const Routes = () => {
   const user = true;
 
-  return user && <AppRoutes />;
+  return user ? <AppRoutes /> : <AuthRoutes />;
 };
 
 export default Routes;
