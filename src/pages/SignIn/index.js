d7e0221e821887588831bfd3a998f272ba0b8366
Luísa Barros <qlullyq@gmail.com>
2020-12-18 18:51:30 -0300

    feat: add user login functionality

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,38 +1,69 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Alert } from 'react-native';
 import PropTypes from 'prop-types';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+
+import { useAuth } from '../../hooks/auth';
 
 // eslint-disable-next-line import/no-cycle
 import SignUp from '../SignUp';
 
 import { Container, Wrapper, Title, SmallButton } from './styles';
 
-const SignIn = ({ setModal }) => (
-  <Container>
-    <Title>Welcome</Title>
+const SignIn = ({ setModal }) => {
+  const { signIn } = useAuth();
 
-    <Wrapper>
-      <Input placeholder="User" />
-      <Input placeholder="Password" />
-    </Wrapper>
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    <Wrapper>
-      <Button mode="dark">Sign in</Button>
-      <SmallButton
-        onPress={() =>
-          setModal({
-            isVisible: true,
-            content: () => <SignUp setModal={setModal} />,
-          })
-        }
-      >
-        new user
-      </SmallButton>
-    </Wrapper>
-  </Container>
-);
+  const handleSubmit = useCallback(async () => {
+    try {
+      await signIn({ email, password });
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  }, [email, password, signIn]);
+
+  return (
+    <Container>
+      <Title>Welcome</Title>
+
+      <Wrapper>
+        <Input
+          value={email}
+          placeholder="User"
+          returnKeyType="next"
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <Input
+          value={password}
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={setPassword}
+        />
+      </Wrapper>
+
+      <Wrapper>
+        <Button mode="dark" onPress={handleSubmit}>
+          Sign in
+        </Button>
+        <SmallButton
+          onPress={() =>
+            setModal({
+              isVisible: true,
+              content: () => <SignUp setModal={setModal} />,
+            })
+          }
+        >
+          new user
+        </SmallButton>
+      </Wrapper>
+    </Container>
+  );
+};
 
 SignIn.propTypes = {
   setModal: PropTypes.func.isRequired,
