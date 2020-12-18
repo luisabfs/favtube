f6cb74de730da78998067765bb670f72e7c43fd9
Luísa Barros <qlullyq@gmail.com>
2020-12-17 23:03:26 -0300

    feat: create user and store in realm database

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Alert } from 'react-native';
 import PropTypes from 'prop-types';
 
 import Button from '../../components/Button';
@@ -7,32 +8,74 @@ import Input from '../../components/Input';
 // eslint-disable-next-line import/no-cycle
 import SignIn from '../SignIn';
 
+import getRealm from '../../services/realm';
+
 import { Container, Wrapper, Title, SmallButton } from './styles';
 
-const SignUp = ({ setModal }) => (
-  <Container>
-    <Title>Create new user</Title>
+const SignUp = ({ setModal }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    <Wrapper>
-      <Input placeholder="User" />
-      <Input placeholder="Password" />
-    </Wrapper>
+  const handleSubmit = useCallback(async () => {
+    try {
+      const realm = await getRealm();
 
-    <Wrapper>
-      <Button mode="dark">Sign up</Button>
-      <SmallButton
-        onPress={() =>
-          setModal({
-            isVisible: true,
-            content: () => <SignIn setModal={setModal} />,
-          })
-        }
-      >
-        login
-      </SmallButton>
-    </Wrapper>
-  </Container>
-);
+      realm.write(() => {
+        realm.create('User', { email, password });
+      });
+
+      Alert.alert(
+        'Successfully registered!',
+        'You can log in the application now.',
+      );
+
+      setModal({
+        isVisible: false,
+        content: () => <SignIn setModal={setModal} />,
+      });
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  }, [email, password, setModal]);
+
+  return (
+    <Container>
+      <Title>Create new user</Title>
+
+      <Wrapper>
+        <Input
+          value={email}
+          placeholder="User"
+          returnKeyType="next"
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        <Input
+          value={password}
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={setPassword}
+        />
+      </Wrapper>
+
+      <Wrapper>
+        <Button mode="dark" onPress={handleSubmit}>
+          Sign up
+        </Button>
+        <SmallButton
+          onPress={() =>
+            setModal({
+              isVisible: true,
+              content: () => <SignIn setModal={setModal} />,
+            })
+          }
+        >
+          login
+        </SmallButton>
+      </Wrapper>
+    </Container>
+  );
+};
 
 SignUp.propTypes = {
   setModal: PropTypes.func.isRequired,
